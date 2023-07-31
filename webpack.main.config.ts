0f5238b3cf631ce5f7b05b923588a1c0be9faac0
owfdr <owfdr@ourworld.center>
2023-07-31 16:13:39 +0900

    feat: add menubar

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,18 +1,24 @@
-import type { Configuration } from 'webpack';
-
-import { rules } from './webpack.rules';
+import type { Configuration } from "webpack";
+import { rules } from "./webpack.rules";
+// eslint-disable-next-line import/default
+import CopyPlugin from "copy-webpack-plugin";
 
 export const mainConfig: Configuration = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   // Put your normal webpack config below here
   module: {
     rules,
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "assets", to: "assets" }],
+    }),
+  ],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
 };
