d787e950f0e7d6af1f14b603039bb1168f512575
owfdr <owfdr@ourworld.center>
2023-07-31 19:53:13 +0900

    feat: add icons

--- src/component/TabBar.tsx
+++ src/component/TabBar.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {
+  GlobeAsiaAustraliaIcon,
+  Cog6ToothIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/outline";
 
 const routes = [
   {
     name: "Explore",
     path: "/",
+    icon: GlobeAsiaAustraliaIcon,
   },
   {
     name: "Settings",
     path: "/settings",
+    icon: Cog6ToothIcon,
   },
   {
     name: "Info",
     path: "/info",
+    icon: InformationCircleIcon,
   },
 ];
 
@@ -29,7 +37,9 @@ export default function TabBar() {
               : "inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
           }
         >
-          {route.name}
+          {React.createElement(route.icon, {
+            className: "w-5 h-5 mb-1 mx-auto",
+          })}
         </NavLink>
       ))}
     </div>
